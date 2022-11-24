d1a1b984525e5eccf0377db61ee18d67c139448b
GBRL-Web <gabi.titi.dnd@gmail.com>
2022-11-24 14:44:47 +0100

    fix: Exercise one name description added.

--- exo1.ts
+++ exo1.ts
@@ -1,6 +1,6 @@
 /**
  * This function just says hello to you!
- * @param name 
+ * @param name - The name to be greeted.
  * @returns String "Hello name!"
  */
 function hello(name:string):string {
---
+++ exo9.ts
@@ -0,0 +1,4 @@
+
+function money(value:number) {
+        
+}
