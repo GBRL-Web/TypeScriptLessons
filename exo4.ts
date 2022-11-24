ef857bd2fbebb74e5302d82e79001cb1c7003e15
GBRL-Web <gabi.titi.dnd@gmail.com>
2022-11-24 12:57:28 +0100

    docs: Fourth exercise.

---
+++ exo4.ts
@@ -0,0 +1,12 @@
+/**
+ *  Simple function to output the highest number from the inputted numbers.
+ * @param a |
+ * @param b } Single numerical values to compare
+ * @param c |
+ * @returns - The highest number from the three values
+ */
+function getMax(a: number, b: number, c:number): number {
+    let numArr: Array<number> = [a, b, c];
+    return Math.max(...numArr);
+}
+console.log("Highest number between 3,7,1 is: ", getMax(3,7,1));
