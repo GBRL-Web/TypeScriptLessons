0000ae3f479e6e1946ef81d41c633b028308d32c
GBRL-Web <gabi.titi.dnd@gmail.com>
2022-11-24 11:39:44 +0100

    docs: Forgot to push after first exercise, so yeah... here you go. Two!

---
+++ exo1.ts
@@ -0,0 +1,9 @@
+/**
+ * This function just says hello to you!
+ * @param name 
+ * @returns String "Hello name!"
+ */
+function hello(name:string):string {
+    return `Hello ${name}!`;    
+}
+console.log(hello('Gabriel'));
---
+++ exo2.ts
@@ -0,0 +1,11 @@
+/**
+ * Small function to calculate two numbers.
+ * @param x - number 1
+ * @param y - number 2
+ * @returns - Number representing the sum of the two numbers.
+ */
+
+function calc(x:number, y:number):number {
+    return x + y;
+}
+console.log("6 + 4 = " + calc(6, 4));
