b6b3f9e1a5924f267a3bc12957b7053acef70659
GBRL-Web <gabi.titi.dnd@gmail.com>
2022-11-24 13:06:43 +0100

    docs: Fifth exercise.

---
+++ exo5.ts
@@ -0,0 +1,10 @@
+/**
+ * Simple comparison function to get the highest value of an array of multiple values.
+ * @param number - Input of multiple numerical values. By default is an array.
+ * @returns - Highest numerical value from the input array of numbers.
+ */
+function getHighest(...number:number[]):number {
+    return Math.max(...number);
+}
+
+console.log("Highest number between 3,5,1,2,4,9,10 is: " + getHighest(3,5,1,2,4,9,10));
