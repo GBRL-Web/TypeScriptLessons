f688525995578593f78a61d05afb5ea64751d2ee
GBRL-Web <gabi.titi.dnd@gmail.com>
2022-11-28 10:51:47 +0100

    docs: Ex. 10 finished. :sparkles:

--- exo10.ts
+++ exo10.ts
@@ -1,7 +1,12 @@
 
-function lowestTemp(...temp) {
-    let lowestTemp: number = 0;
+function lowestTemp(...temp:number[]):number {
+    let lowTemp: number = Math.max(...temp);
     for (let i = 0; i < temp.length; i++) {
-        
+        if (lowTemp > Math.abs(temp[i])) {
+            lowTemp = temp[i];
+        }
     }
+    return lowTemp;
 }
+
+console.log("Closest temperature to 0 from [12, 1, -1, -15, 5] is: " + lowestTemp(12, 1, -1, -15, 5));
