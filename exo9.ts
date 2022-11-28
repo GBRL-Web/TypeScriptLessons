0a432225cfa3f4f326f6338b0642ae059f483f42
GBRL-Web <gabi.titi.dnd@gmail.com>
2022-11-28 10:42:18 +0100

    docs: Ex. 9 finished.

--- exo9.ts
+++ exo9.ts
@@ -1,8 +1,29 @@
 //in development.
 
 function money(inputAmount: number) {
-  const moneyBills: Array<number> = [
-    500, 200, 20, 10, 5, 2, 0.5, 0.02, 0.01
-  ];
+  let amount: number = inputAmount;
+  let moneyChanged: { [key: string]: any} = {};
+  let billsToReturn: Array<number> = [];
+  const moneyBills: number[] = [500, 200, 20, 10, 5, 2, 0.5, 0.02, 0.01];
 
+  while (amount > 0) {
+    for (let bill of moneyBills) {
+      if (bill <= amount) {
+        amount = amount - bill;
+        amount = Math.round(amount * 100) / 100;
+        billsToReturn.push(bill);
+        break;
+      }
+    }
+  }
+
+  for (const element of billsToReturn) {
+    moneyChanged[element] = moneyChanged[element] ? moneyChanged[element] + 1 : 1;
+  }
+  console.log(`For $${inputAmount} :`);
+  for (const [key, value] of Object.entries(moneyChanged)) {
+    console.log(`$${key} x ${value}`);
+  }
 }
+
+money(3200);
