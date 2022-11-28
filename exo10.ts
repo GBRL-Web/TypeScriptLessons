a76861d6cc45d3b0b126cc0427f775dcbae71360
GBRL-Web <gabi.titi.dnd@gmail.com>
2022-11-28 11:01:17 +0100

    docs: ex. 10 added comments and description. :sparkles:

--- exo10.ts
+++ exo10.ts
@@ -1,4 +1,8 @@
-
+/**
+ * Get any amount of values from the user and checks for the closest value to 0.
+ * @param temp - Array of temperatures to compare.
+ * @returns - Closest temperature to 0
+ */
 function lowestTemp(...temp:number[]):number {
     let lowTemp: number = Math.max(...temp);
     for (let i = 0; i < temp.length; i++) {
